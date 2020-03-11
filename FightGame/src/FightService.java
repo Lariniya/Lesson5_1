a4ce532fa3d01f9c9eb2955aa4fd8f06be567d00
Svetlana <svetlana.polyakova@dataart.com>
2020-03-11 21:04:34 +0300

    fix naming variables

--- FightGame/src/FightService.java
+++ FightGame/src/FightService.java
@@ -5,16 +5,16 @@ public class FightService {
     public void fight(Unit unit1, Unit unit2) {
         Random random = new Random(System.currentTimeMillis());
         while (unit1.getHealth() > 0 && unit2.getHealth() > 0){
-            int physical_damage1 = unit1.getPhysical_Damage();
-            int magic_damage1 = unit1.getMagic_Damage();
-            unit2.physicalHit(physical_damage1);
-            System.out.println(unit1.getName() + " нанес " + physical_damage1 + " физического урона " + unit2.getName());
+            int physicalDamage1 = unit1.getPhysicalDamage();
+            int magic_damage1 = unit1.getMagicDamage();
+            unit2.physicalHit(physicalDamage1);
+            System.out.println(unit1.getName() + " нанес " + physicalDamage1 + " физического урона " + unit2.getName());
             if(random.nextInt(100) < 30){
                 unit2.magicHit(magic_damage1);
                 System.out.println(unit1.getName() + " нанес " + magic_damage1 + " магического урона " + unit2.getName());
             }
-            int physical_damage2 = unit2.getPhysical_Damage();
-            int magic_damage2 = unit2.getMagic_Damage();
+            int physical_damage2 = unit2.getPhysicalDamage();
+            int magic_damage2 = unit2.getMagicDamage();
             unit1.physicalHit(physical_damage2);
             System.out.println(unit2.getName() + " нанес " + physical_damage2 + " физического урона " + unit1.getName());
             if(random.nextInt(100) < 30){
